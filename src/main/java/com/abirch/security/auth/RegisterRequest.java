ef2e551053038693693ceefabd392068d79e7562
Ab.ch <Choucheneabir7@gmail.com>
2025-05-12 12:08:54 +0100

    ajout entity role

--- src/main/java/com/abirch/security/auth/RegisterRequest.java
+++ src/main/java/com/abirch/security/auth/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.abirch.security.auth;
 
+import com.abirch.security.user.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
---
+++ src/main/java/com/abirch/security/user/Role.java
@@ -0,0 +1,4 @@
+package com.abirch.security.user;
+
+public class Role {
+}
